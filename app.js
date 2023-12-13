e833620ab52a82fb42f7f6123af278d87c466ba8
Saverio Negro <138687724+saverio-negro@users.noreply.github.com>
2023-12-13 16:55:15 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const smallImages = document.querySelectorAll(".img-container img")
+const modal = document.querySelector(".modal")
+const fullImage = document.querySelector(".full-img")
+
+smallImages.forEach((smallImage) => {
+    smallImage.addEventListener("click", (e) => {
+        smallImageNumber = e.target.title
+        fullImage.src = `./images/large/large_${smallImageNumber}.jpg`
+        setTimeout(() => {modal.setAttribute("class", "modal open")}, 0)
+        fullImage.setAttribute("class", "full-img open")
+    })
+})
+
+modal.addEventListener("click", () => {
+    modal.setAttribute("class", "modal")
+    fullImage.setAttribute("class", "full-img")
+})

40d5a61c7e4ba7ad108339fb2085bab42c4812d9
Saverio Negro <138687724+saverio-negro@users.noreply.github.com>
2023-12-13 17:55:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ smallImages.forEach((smallImage) => {
     smallImage.addEventListener("click", (e) => {
         smallImageNumber = e.target.title
         fullImage.src = `./images/large/large_${smallImageNumber}.jpg`
-        setTimeout(() => {modal.setAttribute("class", "modal open")}, 300)
+        setTimeout(() => {modal.setAttribute("class", "modal open")}, 200)
         fullImage.setAttribute("class", "full-img open")
     })
 })
